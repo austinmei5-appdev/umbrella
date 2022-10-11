0edf6f24d19377074f325bab205aa18cba6981e4
austinmei5 <austinmei@uchicago.edu>
2022-10-11 23:14:23 +0000

    added gmaps api

--- umbrella.rb
+++ umbrella.rb
@@ -1,5 +1,31 @@
+require "open-uri"
+require "json"
+
 p "Hi! Where are you located?"
 
 user_location = gets.chomp
 
+# user_location = "Taj Mahal"
+
 p user_location
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U"
+
+/ look up ruby dig method / 
+
+raw_data = URI.open(gmaps_url).read
+
+parsed_data = JSON.parse(raw_data)
+
+results =  parsed_data.fetch("results")
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
+p latitude
+p longitude
